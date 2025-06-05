3ef683fdfd60e86bfcd874ebe9cc2feff07b0d47
Ali Ahmad <usamamayo.dev@gmail.com>
2025-06-05 12:32:23 +0500

    chat app

---
+++ index.js
@@ -0,0 +1,32 @@
+const { Socket } = require('dgram');
+const express=require('express');
+const http=require('http');
+const path=require ('path');
+const { Server } = require('socket.io');
+
+const app=express();
+const server=http.createServer(app);
+const io=new Server(server);
+
+io.on('connection', (socket)=>{
+    socket.on("user-message" ,(message)=>{
+        socket.broadcast.emit('message', message);
+
+    });
+})
+
+app.use(express.static(path.resolve('./public')));
+
+
+app.get("/",(req,res)=>{
+    return res.sendFile('/public/index.html')
+});
+
+
+
+
+server.listen(3000,()=>{
+    console.log('server is running on port 3000')
+})
+
+
